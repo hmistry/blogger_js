0095a643f3ae3ea2a742dc7b03203c94d6f12a4a
hiren mistry <hiren.mistry@chai-monsters.com>
2013-12-28 17:51:11 -0800

    I3: Added Tagging

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,15 @@
 class Article < ActiveRecord::Base
   has_many :comments
+  has_many :taggings
+  has_many :tags, through: :taggings
+
+
+  def tag_list
+    tags.join(", ")
+  end
+
+  def tag_list=(tag_string)
+    tag_names = tag_string.split(",").collect {|s| s.strip.downcase}.uniq
+    self.tags = tag_names.collect {|t| Tag.find_or_create_by(name: t)}
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Blogger::Application.routes.draw do
     resources :comments
   end
 
+  resources :tags
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
